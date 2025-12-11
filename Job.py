a9ecccce131e66551c89211a465c950c756c25ac
wsfan <c5029191@newcastle.ac.uk>
2025-12-11 18:19:34 +0000

    Added template "job_manager.py" Implemented own code to fufill the functionality of all methods

--- Job.py
+++ Job.py
@@ -36,3 +36,94 @@ class job:
     def __repr__(self):
         return self.__str__()
 
+import csv
+from job import job
+class jobManager:
+
+    def __init__(self, jobs=None):
+        if jobs is None:
+            self._jobs = []
+        else:
+            self._jobs = jobs
+
+    def get_jobs(self):
+        return self._jobs
+
+    def __str__(self):
+        return f"jobManager({self._jobs})"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def _is_worker_available(self, job):
+        return job not in self._jobs
+
+    def add_job(self, job):
+        if self._is_worker_available(job):
+            self._jobs.append(job)
+            return True
+        return False
+
+    def remove_job(self, job):
+        if job in self._jobs:
+            self._jobs.remove(job)
+            return True
+        return False
+
+    def edit_job(self, old_job, new_job):
+        if old_job not in self._jobs:
+            return False
+        if new_job in self._jobs and new_job != old_job:
+            return False
+
+        index = self._jobs.index(old_job)
+        self._jobs[index] = new_job
+        return True
+
+    def search_by_category(self, category):
+        return [job for job in self._jobs if job.get_category() == category]
+
+    def search_by_rate(self, rate):
+        return [job for job in self._jobs if job.get_rate() == rate]
+
+    def search_by_name_and_date(self, name, date):
+        return [job for job in self._jobs if job.get_worker_name() == name and job.get_date() == date]
+
+    def get_total_cost_per_name(self):
+        result = {}
+        for job in self._jobs:
+            name = job.get_worker_name()
+            cost = job.get_rate() * job.get_hours()
+            if name not in result:
+                result[name] = 0
+            result[name] += cost
+        return result
+
+    def get_category_count_per_name(self):
+        result = {}
+        for job in self._jobs:
+            name = job.get_worker_name()
+            category = job.get_category()
+
+            if name not in result:
+                result[name] = {}
+
+            if category not in result[name]:
+                result[name][category] = 0
+
+            result[name][category] += 1
+
+        return result
+
+    def load_from_file(self, file_name):
+        with open(file_name, newline='') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                name, category, rate, date, hours = row
+                job = job(name, category, float(rate), date, int(hours))
+
+    def save_to_file(self, file_name):
+        with open(file_name, "w", newline='') as file:
+            writer = csv.writer(file)
+            for job in self._jobs:
+                writer.writerow([job.get_name(), job.get_category(), job.get_rate(), job.get_date(), job.get_hours()])
